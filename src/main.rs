96ad2720bf7b4551251dd906cba882d997c69ba9
Nick Bryan <nickbryan.dev@gmail.com>
2021-07-26 10:30:53 +0100

    Add health check

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![deny(clippy::all, clippy::pedantic)]
 
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    run(TcpListener::bind("127.0.0.1:8420").expect("failed to bind address"))?.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,28 @@
+use std::net::TcpListener;
+
+#[actix_rt::test]
+async fn health_check_works() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&format!("{}/health-check", &address))
+        .send()
+        .await
+        .expect("failed to execute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    // Port 0 is special-cased at the OS level: trying to bind port 0 will trigger an OS scan
+    // for an available port which will then be bound to the application.
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("failed to bind address");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
